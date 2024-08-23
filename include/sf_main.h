71e66a3a180aacf87598d922dd44647f4b8b8d70
kauduc <kauduc@umr.fr>
2024-08-23 11:20:06 +0200

    feat: add load_arguments function and update CMakeLists.txt

--- include/sf_main.h
+++ include/sf_main.h
@@ -3,7 +3,8 @@
 
 #include <iostream>
 
-using namespace std;
+#include "sf_global.h"
 
+using namespace std;
 
 #endif // SF_MAIN_H
--- src/sf_main.cpp
+++ src/sf_main.cpp
@@ -1,5 +1,9 @@
 #include "../include/sf_main.h"
 
 int main(int argc, char** argv) {
-    
+    if (!global_process::load_arguments(argc, argv)) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
